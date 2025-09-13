a04f16944d6f4606137d2ae0137cba58aaca158e
albqvictor1508 <albq.victor@gmail.com>
2025-09-13 18:54:43 -0300

    style: fix indentation and change server log color to green

--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,13 @@ const { APP_NAME, PORT } = env;
 await routify(app);
 
 app.listen({ port: PORT }, (err) => {
-	if (err) return err;
+  if (err) return err;
 
-	const name = APP_NAME.replace(
-		APP_NAME.charAt(0),
-		APP_NAME.charAt(0).toUpperCase(),
-	);
+  const name = APP_NAME.replace(
+    APP_NAME.charAt(0),
+    APP_NAME.charAt(0).toUpperCase(),
+  );
 
-	(app as any).readyAt = new Date();
-	console.log(chalk.yellow(`${name} server running on :${PORT}`));
+  (app as any).readyAt = new Date();
+  console.log(chalk.green(`${name} server running on :${PORT}`));
 });
